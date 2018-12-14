a12d9ae7a9f24b7183864fdfcab40eb23f2f9fc1
Ariel Ahdoot <arieli612@aol.com>
2018-12-13 20:17:33 -0500

    Update weather_data.py
    formatting

--- weather_data.py
+++ weather_data.py
@@ -6,26 +6,22 @@ def read_and_write(file,output_file,errors_file):
 	output_file.write("Location,Temperature,Wind Speed,Weather Description")
 	errors_file.write("Location")
 	file.readline()
-	for line in file:	
-		url = "http://api.openweathermap.org/data/2.5/weather?q={},US&APPID=????????????&units=imperial".format(line.strip())
+	for line in file:
+		name = line.strip()	
+		url = "http://api.openweathermap.org/data/2.5/weather?q={},US&APPID=??????????????&units=imperial".format(name)
 		response = requests.get(url)
 		json_data = json.loads(response.text)
 		if json_data['cod'] == 200:
 			if validate(json_data['id'],json_data['name']) == True:
 				output_file.write("\n{},{},{},{}".format(json_data['name'], json_data['main']['temp'], json_data['wind']['speed'], json_data['weather'][0]['description']))
-			else:
-		 		errors_file.write("\n{}".format(json_data['name']))
-		else:
-			errors_file.write("\n{}".format(line.strip()))
+				continue	
+		errors_file.write("\n{}".format(name))
 
-def validate(id,name):
-	url = "http://api.openweathermap.org/data/2.5/weather?id={}&APPID=046bd27d3183cf3d21c91fb02eb86f4f".format(id);
+def validate(loc_id,name):
+	url = "http://api.openweathermap.org/data/2.5/weather?id={}&APPID=??????????????".format(loc_id);
 	response = requests.get(url)
 	json_data = json.loads(response.text)
-	if name != json_data['name']:
-		return False
-	else:
-		return True
+	return True if name == json_data['name'] else False
 
 if __name__ == '__main__':
 	try:
